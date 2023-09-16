548eaee5aeedc2f73ca73e28016dddb8b998223d
Tomokatsu Iguchi <github@igtm.link>
2023-09-16 13:17:33 +0900

    support color snippet support box-shadow etc..
    Co-authored-by: ym-darake <sakamoto.takumi.dev@gmail.com>
    Co-authored-by: Gyo Tamura <35870680+t-gyo@users.noreply.github.com>

---
+++ src/data/colorList.ts
@@ -0,0 +1,11 @@
+export const colorList = {
+  "background-color:[color];": "bg",
+  "color:[color];": "text",
+  "border-color:[color];": "border",
+  "outline-color:[color];": "outline",
+  "text-decoration-color:[color];": "decoration",
+  "accent-color:[color];": "accent",
+  "caret-color:[color];": "caret",
+  "fill:[color];": "fill",
+  "stroke:[color];": "stroke",
+};
